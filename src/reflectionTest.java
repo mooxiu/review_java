1cd9dbcca5824558f3c762a3ba320b0f7452cb0b
mooxiu <monetshaw@outlook.com>
2022-06-14 00:14:29 +0800

    refelect update

--- src/reflectionTest.java
+++ src/reflectionTest.java
@@ -1,21 +1,32 @@
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 
 public class reflectionTest {
-    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchFieldException {
         RefTest e = new RefTest();
         // class type hold runtime type identification
         Class cl = e.getClass();
         // class name
         System.out.println(cl.getName());
         // class methods
+        System.out.println("---------------get methods:");
         for (var i : cl.getMethods()) {
             System.out.println(i.getName());
+        }
+        System.out.println("---------------get declared methods:");
+        for (var j : cl.getDeclaredMethods()) {
+            System.out.println(j.getName());
         }
         System.out.println("=========");
         // class fields
         // can only get public field, not private field
+        System.out.println("--------------get fields");
         for (var i : cl.getFields()) {
             System.out.println(i.getName());
+        }
+        System.out.println("--------------get declared fields");
+        for (var j: cl.getDeclaredFields()) {
+            System.out.println(j.getName());
         }
 
 
@@ -35,11 +46,10 @@ public class reflectionTest {
         System.out.println("===========array class name==========");
         var a = new RefTest[100];
         System.out.println(a.getClass().getName());
-
     }
 }
 
-class RefTest {
+class RefTest extends RefTestFather{
     private int field1;
     public int field2;
 
@@ -55,4 +65,8 @@ class RefTest {
     public void setField1(int field1) {
         this.field1 = field1;
     }
+}
+
+class RefTestFather {
+    public int field3;
 }
