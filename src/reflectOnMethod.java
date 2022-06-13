d4e8aee11de0187e512d4bf989200e490fce43b5
mooxiu <monetshaw@outlook.com>
2022-06-14 00:47:12 +0800

    refelect method invoke

---
+++ src/reflectOnMethod.java
@@ -0,0 +1,19 @@
+import java.lang.reflect.InvocationTargetException;
+import java.util.Random;
+
+/*
+Use reflect to invoke method
+ */
+public class reflectOnMethod {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        var cl = new RefTest2();
+        Object r = cl.getClass().getMethod("guessNumber").invoke(cl);
+        System.out.println((double) r);
+    }
+}
+
+class RefTest2 {
+    public double guessNumber() {
+        return Math.floor(10000 * Math.random());
+    }
+}
